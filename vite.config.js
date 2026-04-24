4f8d904bd1064a4a29144727e70cfc4c0f2add66
JosephBorodach <joseph.borodach@viam.com>
2026-04-24 14:30:12 -0400

    wip

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import glsl from "vite-plugin-glsl";
 
 export default defineConfig({
   base: "./",
-  plugins: [svelte()],
+  plugins: [svelte(), glsl()],
   assetsInclude: ["**/*.hdr"],
   optimizeDeps: {
     esbuildOptions: {
