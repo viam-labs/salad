802a530e73f6aff85e08e99fee90357151e5fe27
JosephBorodach <joseph.borodach@viam.com>
2026-05-08 15:28:15 -0400

    plan motion against gripper instead of arm

--- grabber_controls.go
+++ grabber_controls.go
@@ -294,7 +294,7 @@ func (s *grabberControls) moveArm(ctx context.Context, dest spatialmath.Pose, us
 	pt := dest.Point()
 	s.logger.Infof("moving arm to x=%.2f y=%.2f z=%.2f", pt.X, pt.Y, pt.Z)
 	_, err := s.motionService.Move(ctx, motion.MoveReq{
-		ComponentName: s.arm.Name().ShortName(),
+		ComponentName: s.gripper.Name().ShortName(),
 		Destination:   referenceframe.NewPoseInFrame(referenceframe.World, dest),
 		WorldState:    worldState,
 		Constraints:   constraints,
